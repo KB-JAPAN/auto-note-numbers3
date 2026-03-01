a1a64dac55de5fa880d169f75e23c59623458ecb
KB-JAPAN <teriman21@yahoo.co.jp>
2026-03-01 14:32:03 +0900

    Update auto_pipeline.py

--- auto_pipeline.py
+++ auto_pipeline.py
@@ -1,1 +1,8 @@
-print("GitHub動作テスト成功")
+import os
+
+NOTE_TOKEN = os.getenv("NOTE_ACCESS_TOKEN")
+MANUS_KEY = os.getenv("MANUS_API_KEY")
+
+print("本番コード起動")
+print("NOTE_TOKEN exists:", NOTE_TOKEN is not None)
+print("MANUS_KEY exists:", MANUS_KEY is not None)
